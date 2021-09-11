2c047b5cc1f74022d417fa7e58c9028d7b9d5447
Holger <holger@pirk.name>
2021-09-11 16:48:17 +0100

    add TPC-H Q9

---
+++ Benchmarks/TPCH/Q9.rkt
@@ -0,0 +1,43 @@
+(Sort
+ (Group
+  (Project
+   (
+    (Join
+     'NATION
+     (Join
+      (Join
+       'SUPPLIER
+       (Join
+        (Join
+         (Select 'PART
+                 (Where (StringContains 'P_NAME "green")))
+         'PARTSUPP
+         (Where (Equal ps_partkey p_partkey))
+         )
+        'LINEITEM
+        (Where (And (Equal 'PS_PARTKEY 'L_PARTKEY)
+                    (Equal 'PS_SUPPKEY 'L_SUPPKEY)))
+        )
+       (Where (And
+               (Equal 'S_SUPPKEY 'L_SUPPKEY)
+               (Equal 'S_SUPPKEY 'L_SUPPKEY)
+               ))
+       )
+      'ORDERS
+      (Where (Equal 'O_ORDERKEY 'L_ORDERKEY))
+      )
+     (Where (Equal 'S_NATIONKEY 'N_NATIONKEY))
+     )
+    )
+   (As 'nation 'N_NAME
+       'o_year (Year 'O_ORDERDATE)
+       'amount
+       (Subtract (Multiply 'L_EXTENDEDPRICE (Subtract 1 l_discount))
+                 (Multiply 'PS_SUPPLYCOST 'L_QUANTITY))
+       )
+   )
+  (By 'nation 'O_YEAR)
+  (Sum 'amount)
+  )
+ (By 'nation 'O_YEAR)
+ )
